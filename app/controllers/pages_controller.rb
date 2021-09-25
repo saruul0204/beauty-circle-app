bd261a47b2b640c199113aabccfd222241d6c31c
saruul0204 <saruul0204@yahoo.com>
2021-09-25 11:30:04 +0200

    Add rspec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
   def create_contact
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to contact_path, notice: 'Thank you for contacting us.'
+      redirect_to pages_contact_path, notice: 'Thank you for contacting us.'
 
     else
       render :contact
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :contact do
-    full_name { 'MyString' }
-    email { 'MyString' }
-    message { 'MyText' }
+    full_name { Faker::Name.name }
+    email { Faker::Internet.email }
+    message { Faker::Lorem.paragraphs(number: 1) }
   end
 end
