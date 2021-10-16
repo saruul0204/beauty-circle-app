a2ef1b4237772c26f269a8013c1a5a271d8261f0
saruul0204 <saruul0204@yahoo.com>
2021-10-16 12:07:25 +0200

    Add validations

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Business < ApplicationRecord
+  validates :name, :description, :address, :city, :country, :open_hour,
+            :close_hour, :phone_number, :email, presence: true
 end
