450388fcd83f1ffc6456ee7760d906cfb9f5b881
saruul0204 <saruul0204@yahoo.com>
2021-10-16 09:21:14 +0200

    Create Business model

---
+++ spec/factories/businesses.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :business do
+    name { "MyString" }
+    description { "MyText" }
+    address { "MyString" }
+    country { "MyString" }
+    city { "MyString" }
+    open_hour { "MyString" }
+    close_hour { "MyString" }
+    phone_number { "MyString" }
+    email { "MyString" }
+    facebook_url { "MyString" }
+    instagram_url { "MyString" }
+  end
+end
