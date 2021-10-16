096d4200b4578e31c69bdb41c2828ba6a7e4071a
saruul0204 <saruul0204@yahoo.com>
2021-10-16 11:50:03 +0200

    Seed db with users and businesses

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class BusinessesController < ApplicationController
+  def index
+    @businesses = Business.all
+  end
+
+  def show
+    @business = Business.find(params[:id])
+  end
+
+
+
+
+end
--- spec/factories/businesses.rb
+++ spec/factories/businesses.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :business do
-    name { "MyString" }
-    description { "MyText" }
-    address { "MyString" }
-    country { "MyString" }
-    city { "MyString" }
-    open_hour { "MyString" }
-    close_hour { "MyString" }
-    phone_number { "MyString" }
-    email { "MyString" }
-    facebook_url { "MyString" }
-    instagram_url { "MyString" }
+    name { Faker::Company.name }
+    description { Faker::Lorem.paragraphs(number: 1) }
+    address { Faker::Address.street_address }
+    country { Faker::Address.country }
+    city { Faker::Address.city }
+    open_hour { '8:00 am' }
+    close_hour { '6:00 pm' }
+    phone_number { Faker::PhoneNumber.phone_number_with_country_code }
+    email { Faker::Internet.email }
+    facebook_url { 'https://www.facebook.com/' }
+    instagram_url { 'https://www.instagram.com/' }
   end
 end
