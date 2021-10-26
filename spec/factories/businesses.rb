5bfaa5efd11a64fe393b3a53c4c8026d8ce60618
saruul0204 <saruul0204@yahoo.com>
2021-10-26 15:04:24 +0200

    Add views for show action

--- spec/factories/businesses.rb
+++ spec/factories/businesses.rb
@@ -3,13 +3,13 @@
 FactoryBot.define do
   factory :business do
     name { Faker::Company.name }
-    description { Faker::Lorem.paragraphs(number: 1) }
+    description { Faker::Lorem.paragraphs(number: 2) }
     address { Faker::Address.street_address }
     country { Faker::Address.country }
     city { Faker::Address.city }
     open_hour { '8:00 am' }
     close_hour { '6:00 pm' }
-    phone_number { Faker::PhoneNumber.phone_number_with_country_code }
+    phone_number { Faker::PhoneNumber.cell_phone_with_country_code }
     email { Faker::Internet.email }
     facebook_url { 'https://www.facebook.com/' }
     instagram_url { 'https://www.instagram.com/' }
