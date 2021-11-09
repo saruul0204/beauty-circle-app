f39a0a8b9eb79f1edb86d91d555caccd0c92e6c9
saruul0204 <saruul0204@yahoo.com>
2021-11-09 16:46:44 +0100

    Add link to businesses by city

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'factory_bot_rails'
 include FactoryBot::Syntax::Methods
 
 USERS = rand(10..15)
+CITIES = %w[Skopje Ohrid Struga Bitola Stip Strumica Kumanovo Tetovo Gostivar]
 
 puts 'Seeding started'
 
@@ -12,7 +13,7 @@ USERS.times do |i|
   user = i.zero? ? create(:user, email: 'user@test.com') : create(:user)
 
   # Create couple businesses per user
-  rand(1..3).times { create(:business, user: user) }
+  rand(1..3).times { create(:business, user: user, city: CITIES.sample) }
 end
 
 puts 'Seeding completed'
