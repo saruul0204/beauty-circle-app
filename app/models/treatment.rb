9047e83ff074dd1a3e9e06cff94fa9b34f326050
saruul0204 <saruul0204@yahoo.com>
2021-11-13 10:50:47 +0100

    Add validations and add specs for it

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Treatment < ApplicationRecord
+  validates :name, :description, :price, :duration, presence: true
+
   belongs_to :business
 end
--- spec/models/treatment_spec.rb
+++ spec/models/treatment_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Treatment, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:business) }
   end
+
+  describe 'validations' do
+    it { expect(treatment).to be_valid }
+
+    %i[name description price duration].each do |field_name|
+      it { expect(treatment).to validate_presence_of(field_name) }
+    end
+  end
 end
