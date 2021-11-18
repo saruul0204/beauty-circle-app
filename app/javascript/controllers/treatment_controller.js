bfbb46fa5b7de1df12393d36bd2d6950380fdb31
saruul0204 <saruul0204@yahoo.com>
2021-11-18 19:12:40 +0100

    Add nested form controller

--- app/javascript/controllers/treatment_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus"
-
-export default class extends Controller {
-  static targets = [ "form" ]
-
-  showForm(){
-    this.formTarget.classList.toggle("hidden")
-  }
-}
