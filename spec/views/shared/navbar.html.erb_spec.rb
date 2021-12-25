adf9e7ac8c8215c90bf879fd94b8b344098d802d
saruul0204 <saruul0204@yahoo.com>
2021-12-25 10:21:49 +0100

    Fix rspec for navbar

--- spec/views/shared/navbar.html.erb_spec.rb
+++ spec/views/shared/navbar.html.erb_spec.rb
@@ -3,13 +3,15 @@
 require 'rails_helper'
 
 describe 'shared/navbar' do
-  # before { I18n.available_locales = %i[en mk] }
+  before do
+    controller.request.path_parameters[:controller] = 'pages'
+    controller.request.path_parameters[:action] = 'home'
+  end
 
   it 'displays navbar links' do
-    # assign(, [])
-
     render partial: 'shared/navbar'
 
     expect(rendered).to include('Browse salons', 'About', 'Contact us', 'Sign in', 'Sign up')
+    I18n.available_locales.each { |l| expect(rendered).to include(url_for(locale: l)) }
   end
 end
