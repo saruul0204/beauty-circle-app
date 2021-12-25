c77319472cbb8afa5afc7bb9d5ed7f3e0514ccf4
saruul0204 <saruul0204@yahoo.com>
2021-12-25 11:29:01 +0100

    Implement active storage for cities

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  link_to_files = Dir[Rails.root.join('spec/fixtures/cities/*')]
+  attach_new_images = []
+
+  link_to_files.each do |img|
+    attach_new_images.push(Rack::Test::UploadedFile.new(img, 'image/jpeg'))
+  end
+
   factory :city do
     name { %w[Skopje Ohrid Struga Bitola Stip Strumica Kumanovo Tetovo Gostivar].sample }
+    city_image { attach_new_images.sample }
   end
 end
