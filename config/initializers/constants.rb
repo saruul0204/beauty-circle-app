0ba7c2bfe263f5b4cb4bdc7e06b57c267783b3df
saruul0204 <saruul0204@yahoo.com>
2021-12-28 01:32:42 +0100

    City with coresponding city image

---
+++ config/initializers/constants.rb
@@ -0,0 +1,2 @@
+CITY_NAMES = %w[Skopje Ohrid Bitola].freeze
+#%w[Skopje Ohrid Struga Bitola Stip Strumica Kumanovo Tetovo Gostivar].freeze
--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -3,13 +3,16 @@
 FactoryBot.define do
   link_to_files = Dir[Rails.root.join('spec/fixtures/cities/*')]
   attach_new_images = []
+  city_name = CITY_NAMES.sample
 
   link_to_files.each do |img|
     attach_new_images.push(Rack::Test::UploadedFile.new(img, 'image/jpeg'))
   end
 
+  byebug
+
   factory :city do
-    name { %w[Skopje Ohrid Struga Bitola Stip Strumica Kumanovo Tetovo Gostivar].sample }
-    city_image { attach_new_images.sample }
+    name { city_name }
+    city_image { attach_new_images.find { |img| img.original_filename.include?(city_name.downcase) } }
   end
 end
