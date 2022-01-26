0aa61e5b4bf3674afc130363a505709d0975f794
saruul0204 <saruul0204@yahoo.com>
2022-01-26 16:13:08 +0100

    Remove comments

--- spec/policies/dashboard_policy_spec.rb
+++ spec/policies/dashboard_policy_spec.rb
@@ -7,7 +7,6 @@ describe DashboardPolicy do
 
   context 'when a visitor' do
     let(:user) { nil }
-    # let(:business) { create :business }
 
     permissions :home? do
       it 'does not grant access for not logged in visitors' do
@@ -18,7 +17,6 @@ describe DashboardPolicy do
 
   context 'when user is accessing dashboard' do
     let(:user) { create :user }
-    # let(:business) { create(:business, user: user) }
 
     permissions :home? do
       it 'grants access to user' do
