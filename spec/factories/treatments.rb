e601a556b80512236cc0126c4ea2ce8781326ad9
saruul0204 <saruul0204@yahoo.com>
2022-02-01 03:39:16 +0100

    Change treatment currency

--- spec/factories/treatments.rb
+++ spec/factories/treatments.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     association :business, factory: :business
     name { Faker::Lorem.word }
     description { Faker::Lorem.paragraph(sentence_count: 3) }
-    price { Faker::Commerce.price(range: 0..10.0, as_string: true) }
+    price { Faker::Commerce.price(range: 5..30, as_string: true) }
     duration { "#{Faker::Number.between(from: 30, to: 120)} minutes" }
   end
 end
