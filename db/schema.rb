24b0503a9a19fb6df2341c1abc099451e9e06555
saruul0204 <saruul0204@yahoo.com>
2023-02-01 22:21:40 +0100

    Change duration type to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_234828) do
+ActiveRecord::Schema.define(version: 2023_02_01_210627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,7 +133,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_234828) do
     t.string "name"
     t.text "description"
     t.string "price"
-    t.string "duration"
+    t.integer "duration", default: 60
     t.bigint "business_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
