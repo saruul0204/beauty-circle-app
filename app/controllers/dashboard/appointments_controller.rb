0e5d043261085a47e736dc0849c3b3edcb961562
saruul0204 <saruul0204@yahoo.com>
2023-02-06 18:28:46 +0100

    Implement simple calendar gem for viewing appointments, add associations, styles

--- app/controllers/dashboard/appointments_controller.rb
+++ app/controllers/dashboard/appointments_controller.rb
@@ -1,9 +1,55 @@
 # frozen_string_literal: true
 
-class AppointmentsController < InheritedResources::Base
-  private
+module Dashboard
+  class AppointmentsController < Dashboard::DashboardController
+    before_action :set_appointment, only: %i[show edit update destroy]
 
-  def appointment_params
-    params.require(:appointment).permit(:title, :client_name, :client_email, :start_time, :treatment_id, :user_id)
+    def index
+      @appointments = current_user.appointments.all
+    end
+
+    def show
+    end
+
+    def new
+      @appointment = Appointment.new
+      @appointment.user_id = current_user.id
+    end
+
+    def edit
+    end
+
+    def create
+      @appointment = Appointment.new(appointment_params)
+      @appointment.user_id = current_user.id
+      if @appointment.save
+        redirect_to dashboard_appointment_path(@appointment), notice: 'Appointment was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @appointment.update(appointment_params)
+        redirect_to dashboard_appointment_path(@appointment), notice: 'Appointment was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @appointment.destroy
+      redirect_to dashboard_appointments_path, notice: 'Appointment was successfully destroyed.'
+    end
+
+    private
+
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
+    end
+
+    def appointment_params
+      params.require(:appointment).permit(:title, :client_name, :client_email, :start_time, :treatment_id)
+    end
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,8 @@
 class Appointment < ApplicationRecord
   belongs_to :treatment
   belongs_to :user
+
+  def time_name
+    "#{start_time.strftime('%R')}: #{client_name}"
+  end
 end
